3058952f4fad0ead0438f2b863376046a737016d
Trishit Majumdar <123351862+Quantum3600@users.noreply.github.com>
2025-06-27 20:11:58 +0530

    ksp and Room Integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.mycalculator"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.mycalculator"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -27,16 +30,19 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
     }
 }
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
+    }
+}
+
 
 dependencies {
 
@@ -51,6 +57,9 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.material3)
     implementation("androidx.compose.material3:material3-android:1.4.0-alpha16")
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)  // Kotlin Extensions for Room
+    ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
